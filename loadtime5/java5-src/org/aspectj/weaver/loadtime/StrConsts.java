a14363aeb5e5147bf57e0a26cefc7cd82c517886
ymihay <ymihaylenko@gmail.com>
2015-01-21 15:31:37 +0200

    added StrConsts class

---
+++ loadtime5/java5-src/org/aspectj/weaver/loadtime/StrConsts.java
@@ -0,0 +1,19 @@
+/* *******************************************************************
+ * 	Copyright (c) 2012 Contributors.
+ * All rights reserved.
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution and is available at
+ * http://eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Javier Pereira, 
+ *  Abraham Nevada (lucierna)	 initial implementation
+ * ******************************************************************/
+package org.aspectj.weaver.loadtime;
+
+public final class StrConsts {
+	// Strings must be same as in antorcha/agentInstallerSrc/com/smartbear/agent/StrConsts.java
+	public static String strWarAgent = "smartbear.agent.war.active";
+	public static String strJvmAgent = "smartbear.agent.jvm.active";
+}
